4ca01968ff6b3e2e43bffaa5c8990e9c140ea1a5
vishalchouhan2017 <vishalch@msupply.com>
2017-07-22 15:01:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
+   console.log("avnish");
    response.end('Hello World\n');
 }).listen(8081);
 
