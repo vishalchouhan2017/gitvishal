82d4622336bfb4625bc3a2258ac9898c1fb9fea6
vishalchouhan2017 <vishalch@msupply.com>
2017-07-24 18:18:29 +0530

    server

---
+++ appp.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+require("http").createServer(function(request, response){
+  response.writeHeader(200, {"Content-Type": "text/plain"});
+  response.write("Hello World!");
+  response.end();
+}).listen(8080);
