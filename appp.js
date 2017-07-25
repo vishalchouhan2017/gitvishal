d7b612a81207e3a9428a11c124e2714925f6e113
vishalchouhan2017 <vishalch@msupply.com>
2017-07-25 11:12:17 +0530

    app.js

--- appp.js
+++ appp.js
@@ -4,4 +4,5 @@ require("http").createServer(function(request, response){
   response.writeHeader(200, {"Content-Type": "text/plain"});
   response.write("Hello World!");
   response.end();
+  console.log('Listening on port 8080');
 }).listen(8080);
